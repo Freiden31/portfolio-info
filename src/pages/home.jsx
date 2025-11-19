264aae39774f8514e39e92cc001da2f331dfb520
Freiden31 <freidenduliente02@gmail.com>
2025-11-19 17:44:45 +0800

    Update Portfolio

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import profile from '../assets/profile.jpeg'
 import drawing01 from "../assets/drawing01.jpg"
+import Resume from "./resume"
 
 function Home() {
     return (
-        <div className="w-full h-full flex flex-col sm:flex-row items-center justify-center gap-[5rem] font-mono overflow-hidden mt-[2rem] sm:mt-[10rem] ">
-            <div className="image w-60 h-60 sm:w-[33rem] sm:h-[33rem] flex-shrink-0 active:scale-110 transition-transform eas-in-out duration-500">
-                <img src={profile} alt="" className="h-full w-full rounded-4xl object-ceover" />
+        <div className="w-full h-full block sm:flex sm:flex-row items-center justify-center gap-[5rem] font-mono overflow-hidden mt-[2rem] sm:mt-[10rem] ">
+            <div className="image w-full h-60 sm:w-[33rem] sm:h-[33rem] flex-shrink-0 active:scale-110 transition-transform eas-in-out duration-500 flex items-center justify-center">
+                <img src={profile} alt="" className="h-full w-60 sm:w-full rounded-4xl object-ceover" />
             </div>
-            <div className="flex flex-col items-center justify-center mt-[-2rem] sm:mt-0">
+            <div className="flex flex-col items-center justify-center mt-5 sm:mt-0">
                 <div className="w-full flex justify-end mt-[-5rem] ml-[15rem] z-0 hidden sm:flex">
                     <img src={drawing01} alt="" className="h-[13rem] w-[20rem] rotate-347" />
                 </div>
@@ -16,8 +17,8 @@ function Home() {
                     <h1 className=" text-[20px] sm:text-[40px] font-bold">I'm Freiden Duliente</h1>
                     <h1 className="text-[12px] sm:text-[20px] font-bold">an aspiring Junior Software Engineer.</h1>
                 </div>
-                <div className="mt-[2rem] flex flex-col items-center justify-center w-65 sm:w-[30rem] max-w-full">
-                    <p className="text-[12px] sm:text-sm/6">I'm a fourth year Information Technology student in Isabela State University. 
+                <div className="mt-[2rem] flex flex-col items-center justify-center w-full sm:w-[30rem] max-w-full">
+                    <p className="text-[12px] text-justify sm:text-sm/6">I'm a fourth year Information Technology student in Isabela State University. 
                     Exploring technologies for web development for both frontend and backend. Creating web application for innovative works such management system.</p>
                 </div>
                 <div className="flex flex-col items-center justify-center gap-2 mt-[2rem]">
@@ -25,6 +26,9 @@ function Home() {
                     <div className="h-8 w-[15.5rem] text-[14px] sm:text-[16px] sm:w-[15rem] p-5 bg-[#02366dff] flex items-center justify-center rounded-tl-[15px] rounded-br-[15px]"><a href="#" className="text-white">PROJECTS</a></div>
                     <div className="h-8 w-[15.5rem] text-[14px] sm:text-[16px] sm:w-[15rem] p-5 bg-[#02366dff] flex items-center justify-center rounded-tl-[15px] rounded-br-[15px]"><a href="#" className="text-white">HOME</a></div>
                 </div>
+            </div>
+            <div className="block sm:hidden overflow-y-hidden w-full">
+                <Resume className="overflow-y-hidden h-full" />
             </div>
         </div>
     )
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,11 +1,101 @@
 import React from "react";
+import image1 from "../assets/profile.jpeg"
 
-function Resume() {
+function App(){
     return (
-        <div className="h-screen w-screen flex items-center justify-center">
-            Resume
+        <div className="w-full h-screen flex-row items-center justify-center sm:px-[20rem] font-mono gap-5 overflow-x-hidden">
+            <div className="flex-row justify-center text-center mb-5 hidden sm:block">
+                <h2 className="text-[40px] font-bold">Resume.</h2>
+                <div className="w-full flex items-center justify-center mt-5 sm:mt-6">
+                    <p className="text-sm/6 text-center">
+                    I'm a fourth year Information Technology student in Isabela State University. Exploring technologies for web <br/>
+                    development for both frontend and backend. Creating web application for innovative works such management system.</p>
+                </div>
+            </div>
+
+            <div className="flex justify-center grid grid-cols-none sm:grid-cols-3 gap-9 ">
+                {/*information */}
+                <div className="flex-row justify-between gap-5 sm:col-span-2 w-auto sm:w-full">
+                    <div className="flex-row justify-center items-center gap-[3rem] mt-8 mb-8">
+                        <div className="flex items-center justify-center">
+                            <h4 className="text-[20px] sm:text-[25px]">Freiden Duliente</h4>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <div className="flex gap-0 sm:gap-2">
+                                <p className="text-[11px] sm:text-[13px] ">09972106703 | </p>
+                                <p className="text-[11px] sm:text-[13px] "> freidenduliente02@gmail.com </p>
+                                <p className="text-[11px] sm:text-[13px] sm:flex hidden"> | https://freidenduliente.vercel.app</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex-row mb-5">
+                        <div className="flex items-center">
+                            <p className="text-[14px] tracking-[0.1em] font-bold">EDUCATION</p>
+                        </div>
+                         <hr className="h-[2px]  bg-black mb-3"/>
+                         <div className="flex-row items-center">
+                            <div className="flex justify-between items-center gap-9">
+                                <div className="flex items-center">
+                                    <p className="sm:text-[14px] text-[11px]  font-bold text-justify sm:text-left">Isabela State University, College of Engineering, Architecture, and Technology</p>
+                                </div>
+                                <div className="flex ">
+                                    <p className="sm:text-[14px] text-[10px]  font-bold text-right sm:text-left">Ilagan Campus</p>
+                                </div>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <div className="flex items-center">
+                                    <p className="sm:text-[12px] text-[11px] sm:text-left text-justify italic">Bachelor of Science in Information Technology <br/> Major in Networking and Security</p>
+                                </div>
+                                <div className="flex mb-5">
+                                    <p className="sm:text-[12px] text-[10px] sm:text-left text-right italic">August 2026</p>
+                                </div>
+                            </div>
+                         </div>
+                    </div>
+
+                    <div className="flex-row items-center mb-5">
+                        <div className="flex items-center">
+                            <p className="text-[14px]tracking-[0.1em] font-bold">SKILLS</p>
+                        </div>
+                         <hr className="h-[2px] w-full bg-black mb-3"/>
+                         <div className="flex items-center gap-2">
+                            <p className="sm:text-[14px] text-[11px] text-justify sm:text-left"><b>Technologies:</b> Python, Django Framework, Rest API, ReactJS Framework, MYSQL, Machine Learning (Supervised), Scikit-learn, Digital Ocean, Google Cloud Platform, Railway, Heroku, Vercel, and Linux</p>
+                         </div>
+                    </div>
+                    
+                    <div className="flex-row">
+                        <div className="flex items-center">
+                            <p className="text-[14px] tracking-[0.1em] font-bold">EXPERIENCE</p>
+                        </div>
+                        <hr className="h-[2px] w-full bg-black mb-3"/>
+                        <div className="flex-row justify-between items-center">
+                            <div className="flex items-center">
+                                <p className="sm:text-[14px] text-[11px] italic">Commission, Reseacrh, and Development</p>
+                            </div>
+                            <div className="flex-row mt-2">
+                                <ul className="sm:text-[14px] text-[11px] list-disc pl-8">
+                                    <li>Desian</li>
+                                    <li>Desian</li>
+                                    <li>Desian</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* images */}
+                <div className="flex-row sm:cols-span-1 py-8 gap-5">
+                    <div className="hidden sm:flex gap-1">
+                        <img src={image1} className="w-[20rem] h-[12rem]" />
+                        <img src={image1} className="w-[10rem] h-[12rem]" />
+                    </div>
+                    <div className="hidden sm:flex w-full">
+                        <img src={image1} className="w-full h-[12rem]" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Resume;
+export default App;
